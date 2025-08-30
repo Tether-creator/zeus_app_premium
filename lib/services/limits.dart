9e3fab01fcda005dec7c8fc8e3e919de0a43b95c
Tether-creator <gruthzillions@gmail.com>
2025-08-30 06:01:10 +0100

    Create limits.dart

---
+++ lib/services/limits.dart
@@ -0,0 +1,17 @@
+class ZeusLimits {
+  // Transfers
+  static const transferPerTxnNGN = 2_000_000.0;
+  static const transferDailyNGN = 100_000_000.0;
+
+  // Airtime
+  static const airtimePerTxn = 20_000.0;
+  static const airtimeDaily = 100_000.0;
+
+  // Data
+  static const dataPerTxn = 20_000.0;
+  static const dataDaily = 100_000.0;
+
+  // Bills
+  static const billsPerTxn = 100_000.0;
+  static const billsDaily = 500_000.0;
+}
