253764aadee053ed8bad74ee51ba1f4d6643a814
Tether-creator <gruthzillions@gmail.com>
2025-08-31 11:56:36 +0100

    Create limits.dart

---
+++ lib/models/limits.dart
@@ -0,0 +1,17 @@
+class Limits {
+  // Transfers
+  static const int transferPerTxn = 2_000_000; // NGN
+  static const int transferDaily = 100_000_000;
+
+  // Airtime
+  static const int airtimePerTxn = 20_000;
+  static const int airtimeDaily = 100_000;
+
+  // Data
+  static const int dataPerTxn = 20_000;
+  static const int dataDaily = 100_000;
+
+  // Bills
+  static const int billsPerTxn = 100_000;
+  static const int billsDaily = 500_000;
+}
