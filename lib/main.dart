4ff227b7469442340425fd0879c196cdf85d1871
Tether-creator <gruthzillions@gmail.com>
2025-08-31 19:09:10 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'theme.dart';
+import 'state/app_state.dart';
+import 'screens/splash_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/send_screen.dart';
-import 'screens/receipt_screen.dart';
 import 'screens/transactions_screen.dart';
+import 'screens/receipt_screen.dart';
 import 'screens/customer_care_screen.dart';
 import 'screens/airtime_screen.dart';
 import 'screens/data_screen.dart';
@@ -14,25 +16,26 @@ import 'screens/account_opening_screen.dart';
 import 'screens/settings_screen.dart';
 import 'screens/about_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  AppState.instance.init(); // mock balances/limits
   runApp(const ZeusApp());
 }
 
 class ZeusApp extends StatelessWidget {
   const ZeusApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ZEUS Premium',
       debugShowCheckedModeBanner: false,
       theme: buildZeusTheme(),
-      initialRoute: HomeScreen.route,
+      home: const SplashScreen(next: HomeScreen()),
       routes: {
-        HomeScreen.route: (_) => const HomeScreen(),
         SendScreen.route: (_) => const SendScreen(),
-        ReceiptScreen.route: (_) => const ReceiptScreen(),
         TransactionsScreen.route: (_) => const TransactionsScreen(),
+        ReceiptScreen.route: (_) => const ReceiptScreen(),
         CustomerCareScreen.route: (_) => const CustomerCareScreen(),
         AirtimeScreen.route: (_) => const AirtimeScreen(),
         DataScreen.route: (_) => const DataScreen(),
@@ -46,3 +49,4 @@ class ZeusApp extends StatelessWidget {
     );
   }
 }
+
